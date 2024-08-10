2b4885e4748fbb4cdc14dfe8ff80a871f82d041e
sufyan592 <120686129+sufyan592@users.noreply.github.com>
2024-08-11 02:48:20 +0500

    update

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const uri = process.env.DB_CONNECTION;
+const uri = process.env.DB_CONNECTION || 'mongodb+srv://suuf786:2OLVlLVXHaj4qWMR@cluster0.ygrk4.mongodb.net/';
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
