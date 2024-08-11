9d7d8230147cfe67fe582d03aee816ce943df710
sufyan592 <120686129+sufyan592@users.noreply.github.com>
2024-08-11 06:35:34 +0500

    update

--- src/server.js
+++ src/server.js
@@ -6,17 +6,12 @@ const path = require("path");
 const productRoutes = require("./routes/product.route");
 require("./db/db");
 const cors = require("cors");
-app.use(cors({
-  origin: ''*'',
-  credentials: true, 
-}));
 dotenv.config();
-const port = process.env.APP_PORT || 3006;
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+const port = process.env.APP_PORT || 3006;
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/product", productRoutes);
